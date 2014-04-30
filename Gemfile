cb45e44f849484569b9cda139bf1de349d5dc548
j0rgy <johnjorg@gmail.com>
2014-04-30 09:39:55 -0700

    added html escaping, xss security and flash messages

--- Gemfile
+++ Gemfile
@@ -1,8 +1,10 @@
 source 'https://rubygems.org'
+ruby "2.1.0"
 
 gem 'sinatra'
 gem 'haml'
 gem 'data_mapper'
+gem 'sinatra/flash'
 gem 'dm-postgres-adapter', :group => :production
 gem 'dm-sqlite-adapter', :group => :development
 
--- recall.rb
+++ recall.rb
@@ -1,4 +1,6 @@
-%w(sinatra data_mapper haml).each { |lib| require lib }
+%w(sinatra data_mapper haml sinatra/flash).each { |lib| require lib }
+
+enable :sessions
 
 class Note
   include DataMapper::Resource
@@ -15,48 +17,83 @@ configure do
   DataMapper.auto_migrate!
 end
 
+helpers do
+  include Rack::Utils
+  alias_method :h, :escape_html
+end
+
 get '/' do
   @notes = Note.all :order => :id.desc  # Retrieve all the notes from the database
   @title = 'All Notes'
+  if @notes.empty?
+    flash.now[:error] = 'No notes found. Add your first below.'
+  end
   haml :home
 end
 
 post '/' do
   n = Note.create(:content => params[:content], :created_at => Time.now, :updated_at => Time.now)
-  redirect '/'
+  if n.save
+    redirect '/', flash[:notice] = 'Note created successfully.'
+  else
+    redirect '/', flash[:error] = 'Failed to save note.'
+  end
 end
 
 get '/:id' do
   @note = Note.get params[:id]
   @title = "Edit note ##{params[:id]}"
-  haml :edit
+  if @note
+    haml :edit
+  else
+    redirect '/', flash[:error] = "Can't find that note."
+  end
 end
 
 put '/:id' do
   n = Note.get params[:id]
+  unless n
+    redirect '/', flash[:error] = "Can't find that note."
+  end
   n.content = params[:content]
   n.complete = params[:complete] ? 1 : 0
   n.updated_at = Time.now
-  n.save
-  redirect '/'
+  if n.save
+    redirect '/', flash[:notice] = 'Note updated successfully.'
+  else
+    redirect '/', flash[:error] = 'Error updating note.'
+  end
 end
 
 get '/:id/delete' do
   @note = Note.get params[:id]
   @title = "Confirm deletion of note ##{params[:id]}"
-  haml :delete
+  if @note
+    haml :delete
+  else
+    redirect '/', flash[:error] = "Can't find that note."
+  end
 end
 
 delete '/:id' do
   n = Note.get params[:id]
-  n.destroy
-  redirect '/'
+  if n.destroy
+    redirect '/', flash[:notice] = "Note deleted successfully."
+  else
+    redirect '/', flash[:error] = "Error deleting note."
+  end
 end
 
 get '/:id/complete' do
   n = Note.get params[:id]
+  unless n
+    redirect '/', flash[:error] = "Can't find that note."
+  end
   n.complete = n.complete ? 0 : 1 # flip it
   n.updated_at = Time.now
-  n.save
-  redirect '/'
+  if n.save
+    redirect '/', flash[:notice] = "Note marked as complete."
+  else
+    redirect '/', flash[:error] = "Error marking note as complete."
+  end
 end
